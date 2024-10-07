77fa9976d8455a91ca2b9bef1998b5e2fce04d8f
agent87 <arnauldkayonga1@gmail.com>
2024-10-08 00:54:51 +0900

    Added logger module

---
+++ sickle/logger.py
@@ -0,0 +1,26 @@
+import os
+from logging import getLogger, Formatter, StreamHandler, FileHandler, INFO
+
+
+def create_logger(name, log_directory="logs"):
+    logger = getLogger(name)
+    logger.setLevel(INFO)
+
+    # Set the logging format
+    formatter = Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+    # Create a StreamHandler
+    stream_handler = StreamHandler()
+    stream_handler.setFormatter(formatter)
+
+    # Create a FileHandler
+    if not os.path.exists(log_directory):
+        os.makedirs(log_directory)
+    file_handler = FileHandler(os.path.join(log_directory, f"{name}.log"))
+    file_handler.setFormatter(formatter)
+
+    # Add both handlers to the logger
+    logger.addHandler(stream_handler)
+    logger.addHandler(file_handler)
+
+    return logger
